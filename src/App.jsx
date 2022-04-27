852a247d9ea1c4dd2e9dcbc7efb70990f8d14076
ghector14 <gomezhector62@gmail.com>
2022-04-26 17:20:09 -0700

    add component files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Portfolio from './components/Portfolio/Portfolio';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Skills />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+    </>
+  )
+}
+
+export default App
