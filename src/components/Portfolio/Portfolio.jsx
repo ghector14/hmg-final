6df460d05e2a560f4ba10053532d99a8d5f446a1
ghector14 <gomezhector62@gmail.com>
2022-04-29 10:42:48 -0700

    add new code

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/skull.jpg';
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Web app" />
+          </div>
+          <h3>Web App</h3>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Web app" />
+          </div>
+          <h3>Web App</h3>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Web app" />
+          </div>
+          <h3>Web App</h3>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Web app" />
+          </div>
+          <h3>Web App</h3>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Web app" />
+          </div>
+          <h3>Web App</h3>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Web app" />
+          </div>
+          <h3>Web App</h3>
+        </article>
+      </div>
+
+    </section>
+    
   )
 }
 
