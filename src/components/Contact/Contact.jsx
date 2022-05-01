ed62c43d2f4810fa2ce8f8b2fadda0e02033b901
ghector14 <gomezhector62@gmail.com>
2022-05-01 13:29:24 -0700

    add contact form code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './contact.css';
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>gomezhector62@gmail.com</h5>
+            <a href="mailto:gomezhector62@gmail.com">Send message</a>
+          </article>
+        </div>
+
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message'></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
